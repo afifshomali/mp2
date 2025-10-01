cfb7c5716062a0190c0f3f05f9d7c826eb894010
Afif Shomali <shomali2@illinois.edu>
2025-10-01 08:49:20 -0500

    usestate update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Container, Text } from '@mantine/core';
 import styles from './Header.module.css';
